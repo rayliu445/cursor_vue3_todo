d4e29d6357583569987fa09ac9e7009a70316a10
冷冷 <2270033969@qq.com>
2024-10-09 22:23:01 +0800

    Organize recent modifications

---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="mb-6 text-3xl font-bold text-center">JAVA AI 系列课程 Cursor 分享</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.vue', './src/**/*.ts'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('daisyui')
+  ],
+  // 可选：添加 daisyui 配置
+  daisyui: {
+    themes: ["light"], // 这里可以选择您想要使用的主题
+  },
+}
